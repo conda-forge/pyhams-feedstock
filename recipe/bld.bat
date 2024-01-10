3398eae714dcc3d30708d5e47e052ddf5f9b1f1a
Garrett Barter <garrett.barter@nrel.gov>
2024-01-10 13:46:06 -0700

    even closer to scipy now

---
+++ recipe/bld.bat
@@ -0,0 +1,27 @@
+@echo on
+setlocal enabledelayedexpansion
+
+REM Set a few environment variables that are not set due to
+REM https://github.com/conda/conda-build/issues/3993
+set PIP_NO_BUILD_ISOLATION=True
+set PIP_NO_DEPENDENCIES=True
+set PIP_IGNORE_INSTALLED=True
+set PIP_NO_INDEX=True
+set PYTHONDONTWRITEBYTECODE=True
+
+:: see explanation here:
+:: https://github.com/conda-forge/scipy-feedstock/pull/253#issuecomment-1732578945
+set "MESON_RSP_THRESHOLD=320000"
+
+:: -wnx flags mean: --wheel --no-isolation --skip-dependency-check
+%PYTHON% -m build -w -n -x .
+if %ERRORLEVEL% neq 0 (type builddir\meson-logs\meson-log.txt && exit 1)
+
+:: `pip install dist\numpy*.whl` does not work on windows,
+:: so use a loop; there's only one wheel in dist/ anyway
+for /f %%f in ('dir /b /S .\dist') do (
+    REM need to use force to reinstall the tests the second time
+    REM (otherwise pip thinks the package is installed already)
+    pip install %%f --force-reinstall
+    if %ERRORLEVEL% neq 0 exit 1
+)
