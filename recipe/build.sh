aa6622e10d0fdc4f0672a1be946ecd83b95a2431
Garrett Barter <garrett.barter@nrel.gov>
2024-01-10 16:50:58 -0700

    one change did not work

--- recipe/build.sh
+++ recipe/build.sh
@@ -15,9 +15,9 @@ echo "python = '${PREFIX}/bin/python'" >> ${CONDA_PREFIX}/meson_cross_file.txt
 
 # meson-python already sets up a -Dbuildtype=release argument to meson, so
 # we need to strip --buildtype out of MESON_ARGS or fail due to redundancy
-#MESON_ARGS_REDUCED="$(echo $MESON_ARGS | sed 's/--buildtype release //g')"
+MESON_ARGS_REDUCED="$(echo $MESON_ARGS | sed 's/--buildtype release //g')"
 
 # -wnx flags mean: --wheel --no-isolation --skip-dependency-check
-$PYTHON -m build -w -n -x -Csetup-args=${MESON_ARGS// / -Csetup-args=} || (cat build/meson-logs/meson-log.txt && exit 1)
+$PYTHON -m build -w -n -x -Csetup-args=${MESON_ARGS_REDUCED// / -Csetup-args=} || (cat build/meson-logs/meson-log.txt && exit 1)
 
 $PYTHON -m pip install dist/py*.whl
