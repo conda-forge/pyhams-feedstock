fde1a3e5ed31bd61d62c86e23f52f2d04845413b
Garrett Barter <garrett.barter@nrel.gov>
2024-03-31 06:52:05 -0600

    flang is getting salty

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -27,11 +27,9 @@ FOR /F "tokens=* USEBACKQ" %%F IN (`clang.exe -dumpversion`) DO (
 
 REM attempt to match flags for flang as we set them for clang-on-win, see
 REM https://github.com/conda-forge/clang-win-activation-feedstock/blob/main/recipe/activate-clang_win-64.sh
-REM however, -Xflang --dependent-lib=msvcrt currently fails as an unrecognized option, see also
-REM https://github.com/llvm/llvm-project/issues/63741
 set "FFLAGS=-D_CRT_SECURE_NO_WARNINGS -D_MT -D_DLL --target=x86_64-pc-windows-msvc"
 set "LDFLAGS=--target=x86_64-pc-windows-msvc -Xflang --dependent-lib=msvcrt -fuse-ld=lld"
-set "LDFLAGS=%LDFLAGS% -Wl,-defaultlib:%BUILD_PREFIX%/Library/lib/clang/!CLANG_VER:~0,2!/lib/windows/clang_rt.builtins-x86_64.lib"
+REM set "LDFLAGS=%LDFLAGS% -Wl,-defaultlib:%BUILD_PREFIX%/Library/lib/clang/!CLANG_VER:~0,2!/lib/windows/clang_rt.builtins-x86_64.lib"
 
 REM see explanation here:
 REM https://github.com/conda-forge/scipy-feedstock/pull/253#issuecomment-1732578945
