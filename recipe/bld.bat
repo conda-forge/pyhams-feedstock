9cc280d2b85a310a11f5c704ccecbb417c833617
Garrett Barter <garrett.barter@nrel.gov>
2024-05-15 08:21:24 -0600

    no iso

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -38,7 +38,7 @@ REM https://github.com/conda-forge/scipy-feedstock/pull/253#issuecomment-1732578
 set "MESON_RSP_THRESHOLD=320000"
 
 REM -wnx flags mean: --wheel --no-isolation --skip-dependency-check
-%PYTHON% -m pip install --prefix "%PREFIX%" --no-deps . -vv
+%PYTHON% -m pip install --prefix "%PREFIX%" --no-deps --no-build-isolation . -vv
 REM %PYTHON% -m build -w -n -x .
 REM if %ERRORLEVEL% neq 0 (type builddir\meson-logs\meson-log.txt && exit 1)
 
