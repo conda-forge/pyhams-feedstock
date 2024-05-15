5f2d30ca6c6bf700fc9f1da7fe1c98806ec7b490
Garrett Barter <garrett.barter@nrel.gov>
2024-05-15 08:07:16 -0600

    try pip build and install maybe

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -38,14 +38,15 @@ REM https://github.com/conda-forge/scipy-feedstock/pull/253#issuecomment-1732578
 set "MESON_RSP_THRESHOLD=320000"
 
 REM -wnx flags mean: --wheel --no-isolation --skip-dependency-check
-%PYTHON% -m build -w -n -x .
-if %ERRORLEVEL% neq 0 (type builddir\meson-logs\meson-log.txt && exit 1)
+%PYTHON% -m pip install --prefix "%PREFIX%" --no-deps . -vv
+REM %PYTHON% -m build -w -n -x .
+REM if %ERRORLEVEL% neq 0 (type builddir\meson-logs\meson-log.txt && exit 1)
 
 REM `pip install dist\numpy*.whl` does not work on windows,
 REM so use a loop; there's only one wheel in dist/ anyway
-for /f %%f in ('dir /b /S .\dist') do (
+REM for /f %%f in ('dir /b /S .\dist') do (
     REM need to use force to reinstall the tests the second time
     REM (otherwise pip thinks the package is installed already)
-    %PYTHON% -m pip install --prefix "%PREFIX%" --no-deps %%f
-    if %ERRORLEVEL% neq 0 exit 1
-)
+    REM %PYTHON% -m pip install --prefix "%PREFIX%" --no-deps %%f
+    REM if %ERRORLEVEL% neq 0 exit 1
+REM )
